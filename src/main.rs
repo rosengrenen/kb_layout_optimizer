f80b88a11124ff60f1c24bed120b6e728e36a9d6
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2022-10-10 04:38:12 +0200

    reintroduce common letters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use lazy_static::lazy_static;
 use rand::{seq::SliceRandom, thread_rng, Rng};
 use rayon::{
@@ -31,6 +33,16 @@ impl Keyboard {
         }
     }
 
+    fn print_freq(&self, letter_freq: &HashMap<char, f64>) {
+        for y in 0..3 {
+            for x in 0..10 {
+                print!("{:.02} ", letter_freq.get(&self.keys[x][y]).unwrap());
+            }
+
+            println!();
+        }
+    }
+
     fn key_pos(&self, key: char) -> (usize, usize) {
         for x in 0..10 {
             for y in 0..3 {
@@ -57,6 +69,7 @@ const CHARS: [char; 30] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z', ',', '.', '?', ';',
 ];
+const MOST_COMMON_CHARS: [char; 8] = ['e', 't', 'a', 'o', 'i', 'n', 's', 'r'];
 lazy_static! {
     static ref BIGRAMS: Vec<(String, i32)> = include_str!("../data/english_bigrams_1.txt")
         .lines()
@@ -113,21 +126,29 @@ const DVORAK: Keyboard = Keyboard {
 
 // Settings
 const POPULATION_SIZE: usize = 200;
-const GENERATIONS: usize = 1000;
+const GENERATIONS: usize = 500;
 const TOURNAMENT_PROBABILTY: f64 = 0.8;
 const TOURNAMENT_SIZE: usize = 2;
-const MUTATION_PROBABILITY: f64 = 0.05;
+const MUTATION_PROBABILITY: f64 = 1.0 / 22.0;
+const MUTATION_PROBABILITY_COMMON: f64 = 1.0 / 8.0;
 const CROSSOVER_PROBABILITY: f64 = 0.8;
 
 fn generate_individual() -> Keyboard {
+    let mut most_common_chars = MOST_COMMON_CHARS.to_vec();
+    most_common_chars.shuffle(&mut thread_rng());
     let mut chars = CHARS.to_vec();
+    chars.retain(|key| !most_common_chars.contains(key));
     chars.shuffle(&mut thread_rng());
     let mut keyboard = Keyboard::default();
 
     for i in 0..10 {
         keyboard.keys[i][0] = chars.pop().unwrap();
-        keyboard.keys[i][1] = chars.pop().unwrap();
         keyboard.keys[i][2] = chars.pop().unwrap();
+        if i < 4 || i > 5 {
+            keyboard.keys[i][1] = most_common_chars.pop().unwrap();
+        } else {
+            keyboard.keys[i][1] = chars.pop().unwrap();
+        }
     }
 
     keyboard
@@ -150,59 +171,129 @@ fn generate_population() -> Vec<Keyboard> {
 // flera lager, men det måste kosta mer att använda tummen pga koordination och stoppar flowet
 
 // params (with weights):
+//  * dont use the same finger for two keys in row
+const SAME_FINGER_PENALTY: f64 = 2.0;
+
 //  * alternating hands
+const SAME_HAND_PENALTY: f64 = 2.0;
+
 //  * disfavour bottom row
+const BOTTOM_ROW_PENALTY: f64 = 5.0;
+
 //  * rolls, outer to inner finger mostly, more awkward inner to outer direction
+
 //  * finger strength, i.e. pinky is weaker
 //  * hand/finger usage symmetry symmetry
+const FINGER_TARGET_USAGE: [f64; 10] = [0.1, 0.135, 0.135, 0.13, 0.0, 0.0, 0.13, 0.135, 0.135, 0.1];
+
 //  * idle time of fingers
 //  * physical restrictions of fingers in a hand
 //			e.g. one first on top row and the adjacent finger on bottom row on consecutive keys is bad
-fn evaluate_individual(individual: &Keyboard, input: &str) -> f64 {
+fn evaluate_individual(
+    individual: &Keyboard,
+    input: &str,
+    letter_freq: &HashMap<char, f64>,
+) -> f64 {
     let mut prev_finger_index = 0;
     let mut fitness = 0.0;
-    let same_finger_multiplier = 5.0;
-    let top_multipler = 1.0;
-    let bottom_multipler = 2.0;
     let calc_distance = |prev_x: isize, prev_y: isize, x: isize, y: isize| {
-        let y_delta = (prev_y - y).abs();
-        let y_distance: f64 = match y_delta {
-            2 => top_multipler + bottom_multipler,
-            1 => match prev_y {
-                0 => top_multipler,
-                1 => match y {
-                    0 => top_multipler,
-                    2 => bottom_multipler,
-                    _ => unreachable!(),
-                },
-                2 => bottom_multipler,
-                _ => unreachable!(),
-            },
-            0 => 0.0,
-            _ => unreachable!(),
-        };
+        let top = prev_y == 0 || y == 0;
+        let bottom = prev_y == 2 || y == 2;
         let x_distance = (prev_x - x).abs() as f64;
+        let y_distance = match (top, bottom) {
+            (true, true) => 1.0 + BOTTOM_ROW_PENALTY,
+            (true, false) => 1.0,
+            (false, true) => BOTTOM_ROW_PENALTY,
+            (false, false) => 0.0,
+        };
         (x_distance.powi(2) + y_distance.powi(2)).sqrt()
     };
 
+    let mut finger_positions = [
+        (0, 1),
+        (1, 1),
+        (2, 1),
+        (3, 1),
+        (4, 1),
+        (5, 1),
+        (6, 1),
+        (7, 1),
+        (8, 1),
+        (9, 1),
+    ];
+    let mut finger_usage = [0; 10];
+    let mut same_finger_count = 0;
+    let mut same_hand_no_roll_count = 0;
+    let mut prev_key = '-';
     for key in input.chars() {
-        let finger_index = individual.key_finger(key);
+        let finger_idx = individual.key_finger(key);
         let (x, y) = individual.key_pos(key);
         let x = x as isize;
         let y = y as isize;
 
-        let start_y = 1;
-        let start_x = finger_index as isize;
+        let (start_x, start_y) = finger_positions[finger_idx];
 
-        if finger_index == prev_finger_index {
-            let distance = calc_distance(start_x, start_y, x, y);
-            fitness += distance * same_finger_multiplier;
-        } else {
-            let distance = calc_distance(start_x, start_y, x, y);
-            fitness += distance;
+        fitness += calc_distance(start_x, start_y, x, y);
+        if finger_idx == prev_finger_index && prev_key != key {
+            same_finger_count += 1;
         }
 
-        prev_finger_index = finger_index;
+        let same_hand = match (finger_idx, prev_finger_index) {
+            (0..=4, 0..=4) => true,
+            (5..=9, 5..=9) => true,
+            _ => false,
+        };
+
+        let normalize_finger_idx = |finger_idx: usize| {
+            if finger_idx > 4 {
+                9 - finger_idx
+            } else {
+                finger_idx
+            }
+        };
+        let normalized_finger_idx = normalize_finger_idx(finger_idx);
+        let prev_normalized_finger_idx = normalize_finger_idx(prev_finger_index);
+        if same_hand && normalized_finger_idx < prev_normalized_finger_idx {
+            same_hand_no_roll_count += 1;
+        }
+
+        finger_positions[finger_idx] = (x, y);
+        finger_usage[finger_idx] += 1;
+        prev_finger_index = finger_idx;
+        prev_key = key;
+    }
+
+    fitness += same_finger_count as f64 * SAME_FINGER_PENALTY;
+    fitness += same_hand_no_roll_count as f64 * SAME_HAND_PENALTY;
+
+    let total_usage = finger_usage.iter().sum::<i32>() as f64;
+    fitness *= 1.0
+        + finger_usage
+            .into_iter()
+            .enumerate()
+            .map(|(finger_idx, usage)| {
+                (1.0 + (FINGER_TARGET_USAGE[finger_idx] - usage as f64 / total_usage).abs()).powi(3)
+                    - 1.0
+            })
+            .sum::<f64>();
+
+    let mut most_common = letter_freq.into_iter().collect::<Vec<_>>();
+    most_common.sort_by(|a, b| b.1.partial_cmp(a.1).unwrap_or(std::cmp::Ordering::Equal));
+    let mut most_common = most_common
+        .into_iter()
+        .map(|(key, _)| *key)
+        .take(8)
+        .collect::<HashSet<_>>();
+    for x in 0..10 {
+        if x == 4 || x == 5 {
+            continue;
+        }
+
+        most_common.remove(&individual.keys[x][1]);
+    }
+
+    if !most_common.is_empty() {
+        fitness *= 1.0 + most_common.len() as f64;
     }
 
     fitness
@@ -245,10 +336,13 @@ fn cross(first_individual: &Keyboard, second_individual: &Keyboard) -> (Keyboard
     }
 
     // Fill in missing in first
+    let mut first_missing_common_chars = MOST_COMMON_CHARS.to_vec();
     let mut first_missing_chars = CHARS.to_vec();
+    first_missing_chars.retain(|key| !MOST_COMMON_CHARS.contains(key));
     for i in 0..crossover_point {
         let current_key = first_new_individual.keys[i / 3][i % 3];
         first_missing_chars.retain(|&key| key != current_key);
+        first_missing_common_chars.retain(|&key| key != current_key);
     }
 
     for i in crossover_point..30 {
@@ -257,24 +351,35 @@ fn cross(first_individual: &Keyboard, second_individual: &Keyboard) -> (Keyboard
         if first_missing_chars.contains(&second_individual.keys[x][y]) {
             first_new_individual.keys[x][y] = second_individual.keys[x][y];
             first_missing_chars.retain(|&key| key != second_individual.keys[x][y]);
+        } else if first_missing_common_chars.contains(&second_individual.keys[x][y]) {
+            first_new_individual.keys[x][y] = second_individual.keys[x][y];
+            first_missing_common_chars.retain(|&key| key != second_individual.keys[x][y]);
         }
     }
 
+    first_missing_common_chars.shuffle(&mut thread_rng());
     first_missing_chars.shuffle(&mut thread_rng());
 
     for i in crossover_point..30 {
         let x = i / 3;
         let y = i % 3;
         if first_new_individual.keys[x][y] == '-' {
-            first_new_individual.keys[x][y] = first_missing_chars.pop().unwrap();
+            if y == 1 && (x < 4 || x > 5) {
+                first_new_individual.keys[x][y] = first_missing_common_chars.pop().unwrap();
+            } else {
+                first_new_individual.keys[x][y] = first_missing_chars.pop().unwrap();
+            }
         }
     }
 
     // Fill in missing in second
+    let mut second_missing_common_chars = MOST_COMMON_CHARS.to_vec();
     let mut second_missing_chars = CHARS.to_vec();
+    second_missing_chars.retain(|key| !MOST_COMMON_CHARS.contains(key));
     for i in crossover_point..30 {
         let current_key = second_new_individual.keys[i / 3][i % 3];
         second_missing_chars.retain(|&key| key != current_key);
+        second_missing_common_chars.retain(|&key| key != current_key);
     }
 
     for i in 0..crossover_point {
@@ -283,16 +388,24 @@ fn cross(first_individual: &Keyboard, second_individual: &Keyboard) -> (Keyboard
         if second_missing_chars.contains(&first_individual.keys[x][y]) {
             second_new_individual.keys[x][y] = first_individual.keys[x][y];
             second_missing_chars.retain(|&key| key != first_individual.keys[x][y]);
+        } else if second_missing_common_chars.contains(&first_individual.keys[x][y]) {
+            second_new_individual.keys[x][y] = first_individual.keys[x][y];
+            second_missing_common_chars.retain(|&key| key != first_individual.keys[x][y]);
         }
     }
 
+    second_missing_common_chars.shuffle(&mut thread_rng());
     second_missing_chars.shuffle(&mut thread_rng());
 
     for i in 0..crossover_point {
         let x = i / 3;
         let y = i % 3;
         if second_new_individual.keys[x][y] == '-' {
-            second_new_individual.keys[x][y] = second_missing_chars.pop().unwrap();
+            if y == 1 && (x < 4 || x > 5) {
+                second_new_individual.keys[x][y] = second_missing_common_chars.pop().unwrap();
+            } else {
+                second_new_individual.keys[x][y] = second_missing_chars.pop().unwrap();
+            }
         }
     }
 
@@ -307,49 +420,100 @@ fn mutate(individual: &mut Keyboard) {
     let mut rng = rand::thread_rng();
     let mut keys = Vec::new();
     let mut key_positions = Vec::new();
+    let mut common_keys = Vec::new();
+    let mut common_key_positions = Vec::new();
     for x in 0..10 {
         for y in 0..3 {
-            if rng.gen::<f64>() < MUTATION_PROBABILITY {
-                keys.push(individual.keys[x][y]);
-                key_positions.push((x, y));
+            if y == 1 && (x < 4 || x > 5) {
+                if rng.gen::<f64>() < MUTATION_PROBABILITY_COMMON {
+                    common_keys.push(individual.keys[x][y]);
+                    common_key_positions.push((x, y));
+                }
+            } else {
+                if rng.gen::<f64>() < MUTATION_PROBABILITY {
+                    keys.push(individual.keys[x][y]);
+                    key_positions.push((x, y));
+                }
             }
         }
     }
 
     keys.shuffle(&mut rng);
+    common_keys.shuffle(&mut rng);
     for x in 0..10 {
         for y in 0..3 {
             if key_positions.contains(&(x, y)) {
                 individual.keys[x][y] = keys.pop().unwrap();
+            } else if common_key_positions.contains(&(x, y)) {
+                individual.keys[x][y] = common_keys.pop().unwrap();
             }
         }
     }
 }
 
 fn main() {
-    let input = include_str!("../data/eng-uk_web_2202_300K/eng-uk_web_2002_300K-sentences.txt")
+    let raw_input = include_str!("../data/eng-uk_web_2202_300K/eng-uk_web_2002_300K-sentences.txt");
+    let input = raw_input
         .lines()
         .step_by(50)
         .map(|line| {
             let (_, content) = line.split_once("\t").unwrap();
             content
+                .to_lowercase()
                 .chars()
-                .filter(|c| CHARS.contains(c))
+                .filter(|c| c.is_ascii_alphabetic())
                 .collect::<String>()
         })
         .collect::<Vec<_>>()
         .join("");
+    let mut letter_freq: HashMap<char, f64> = HashMap::new();
+    let mut bigrams: HashMap<String, f64> = HashMap::new();
+    let mut prev_c = None;
+    for c in raw_input.chars() {
+        let c = c.to_ascii_lowercase();
+        if !CHARS.contains(&c) {
+            continue;
+        }
 
-    println!("qwerty: {}", evaluate_individual(&QWERTY, &input));
-    println!("colemak: {}", evaluate_individual(&COLEMAK, &input));
-    println!("dvorak: {}", evaluate_individual(&DVORAK, &input));
+        *letter_freq.entry(c).or_default() += 1.0;
+
+        if let Some(prev_c) = prev_c {
+            let bigram = format!("{}{}", prev_c, c);
+            *bigrams.entry(bigram).or_default() += 1.0;
+        }
+
+        prev_c = Some(c);
+    }
+
+    let len: f64 = letter_freq.values().sum();
+    for f in letter_freq.values_mut() {
+        *f /= len;
+    }
+
+    let len: f64 = bigrams.values().sum();
+    for b in bigrams.values_mut() {
+        *b /= len;
+    }
+
+    println!(
+        "qwerty: {}",
+        evaluate_individual(&QWERTY, &input, &letter_freq)
+    );
+    println!(
+        "colemak: {}",
+        evaluate_individual(&COLEMAK, &input, &letter_freq)
+    );
+    println!(
+        "dvorak: {}",
+        evaluate_individual(&DVORAK, &input, &letter_freq)
+    );
 
     let mut population = generate_population();
 
     for g in 0..GENERATIONS {
         let fitnesses = population
             .par_iter()
-            .map(|individual| evaluate_individual(individual, &input))
+            .map(|individual| evaluate_individual(individual, &input, &letter_freq))
             .collect::<Vec<_>>();
         let mut fitnesses_with_index = fitnesses.iter().enumerate().collect::<Vec<_>>();
         fitnesses_with_index.sort_by(|(_, left), (_, right)| {
@@ -387,7 +551,7 @@ fn main() {
     let mut ranked_population = population
         .into_par_iter()
         .map(|individual| {
-            let fitness = evaluate_individual(&individual, &input);
+            let fitness = evaluate_individual(&individual, &input, &letter_freq);
             (individual, fitness)
         })
         .collect::<Vec<_>>();
@@ -395,4 +559,5 @@ fn main() {
         left.partial_cmp(right).unwrap_or(std::cmp::Ordering::Equal)
     });
     ranked_population[0].0.print();
+    ranked_population[0].0.print_freq(&letter_freq);
 }
